9c36b75d4337356346ae524896c5d613942e67dc
sjmawa <sjmawa0000@gmail.com>
2025-11-18 16:17:23 +0600

    ok

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import home
+from core.views import home,search
 urlpatterns = [
     path('', home, name='home'),
+    path('search/', search, name='search'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from events.models import Event
+from clubs.models import Club
+from django.db.models import Q
 # Create your views here.
 def home(request):
     Events= Event.objects.all().order_by('-date')[:3]
     return render(request, 'home.html', {'events': Events})
+
+def search(request):
+    query = request.GET.get('q', '')  # get the search term
+    clubs = Club.objects.none()
+    events = Event.objects.none()
+
+    if query:
+        clubs = Club.objects.filter(
+            Q(name__icontains=query) | Q(description__icontains=query)
+        )
+        events = Event.objects.filter(
+            Q(title__icontains=query) | Q(description__icontains=query)
+        )
+
+    context = {
+        'query': query,
+        'clubs': clubs,
+        'events': events
+    }
+    return render(request, 'search_results.html', context)
